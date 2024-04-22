2a766bf956f4ee656fab59d2fdf5e0f2cd9a8785
Victor Jesus Fernandez Noguer <victorjesusfernandeznoguer@gmail.com>
2024-04-22 08:10:56 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chat-app-ionic',
+  webDir: 'www'
+};
+
+export default config;
